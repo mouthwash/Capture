6e7ba2f8524bac7ca82562cc76f528d1ffa42f09
David Wu <davidwu@davidwu.fios-router.home>
2018-04-15 16:39:39 -0400

    created Fixed Header with changing text based on note pane's name

--- Capture/src/components/NotePanes.js
+++ Capture/src/components/NotePanes.js
@@ -21,37 +21,22 @@ export default class NotePanes extends Component {
     super(props);
     this.state = {
       dataset: [
-        { name: 'Urgent', notes: ['Hi', 'Ok Its Not Bad'] },
-        { name: 'School', notes: [] },
-        { name: 'Useless Things', notes: [] },
-        { name: 'Even more Useless', notes: [] }
-      ]
+        { id: 0, name: 'Urgent', notes: ['Hi', 'Ok Its Not Bad'] },
+        { id: 1, name: 'School', notes: [] },
+        { id: 2, name: 'Useless Things', notes: [] },
+        { id: 3, name: 'Even more Useless', notes: [] }
+      ],
+      paneName: 'Title',
     };
   }
+
+  changedPane = (index) => {
+        this.setState({ paneName: this.state.dataset[index].name });
+      }
 
   renderItem({ item, index }) {
     return (
       <Card style={styles.cardStyle}>
-        <Header style={styles.headerStyle}>
-            <Left style={styles.positionStyle}>
-                <Button transparent>
-                  <Icon type='Feather' name='menu' />
-                </Button>
-            </Left>
-            <Body style={styles.positionStyle}>
-                <Button transparent>
-                    <Text style={styles.textStyle}>{item.name}</Text>
-                </Button>
-            </Body>
-            <Right style={styles.positionStyle}>
-                <Button
-                  transparent
-                  onPress={() => this.props.navigation.navigate('NewNoteScreen')}
-                >
-                <Icon type='Feather' name='plus' />
-                </Button>
-            </Right>
-        </Header>
         <Content>
         {
            item.notes.map((item2, index2) => (
@@ -75,13 +60,35 @@ export default class NotePanes extends Component {
   render() {
     return (
       <Container>
+        <Header style={styles.headerStyle}>
+            <Left style={styles.positionStyle}>
+                <Button transparent>
+                  <Icon type='Feather' name='menu' />
+                </Button>
+            </Left>
+            <Body style={styles.positionStyle}>
+                <Button transparent>
+                    <Text style={styles.textStyle}>{this.state.paneName}</Text>
+                </Button>
+            </Body>
+            <Right style={styles.positionStyle}>
+                <Button
+                  transparent
+                  onPress={() => this.props.navigation.navigate('NewNoteScreen')}
+                >
+                <Icon type='Feather' name='plus' />
+                </Button>
+            </Right>
+        </Header>
         <Carousel
           data={this.state.dataset}
           renderItem={this.renderItem.bind(this)}
           itemWidth={Dimensions.get('window').width}
           sliderWidth={Dimensions.get('window').width}
+          ref={(carousel) => { this.carousel = carousel; }}
+          onSnapToItem={this.changedPane}
         />
-      </Container>
+    </Container>
     );
   }
 }
@@ -100,6 +107,7 @@ const styles = {
     marginBottom: 0,
   },
   headerStyle: {
+    backgroundColor: '#263238',
   },
   positionStyle: {
     flex: 1,
