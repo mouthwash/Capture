60ef3f565e21f1117b93a616102b58e97da197f5
Dan Naeem <dan.naeem95@gmail.com>
2018-04-22 15:57:24 -0400

    consolidated all styles into one sylesheet

---
+++ Capture/src/styles/stylesheet.js
@@ -0,0 +1,75 @@
+//notePane
+const styles = {
+
+  headerStyle: {
+    backgroundColor: '#06317c'
+  },
+
+  cardStyle: {
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 0,
+    marginBottom: 0,
+    backgroundColor: '#e6e9ef',
+  },
+
+  textStyle: {
+    fontSize: 20,
+    color: 'white',
+    textAlign: 'center',
+    margin: 'auto',
+  },
+
+  positionStyle: {
+    flex: 1,
+  },
+
+  iconStyle: {
+    color: 'white'
+  },
+
+  newPaneStyle: {
+    padding: 10,
+  },
+
+  inputStyle: {
+      textAlignVertical: 'top',
+      fontSize: 20,
+      paddingLeft: 15,
+      paddingRight: 15,
+      paddingTop: 20,
+      width: '100%'
+  },
+
+  footerStyle: {
+      backgroundColor: 'transparent',
+  },
+
+  iconStyle: {
+      color: 'white'
+  },
+
+  containerStyle: {
+    flex: 1,
+    alignItems: 'flex-start',
+  },
+
+  buttonStyle: {
+    position: 'absolute',
+    paddingLeft: 20,
+    paddingBottom: 20,
+    bottom: 0,
+  },
+
+  datePicker: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 350,
+  }
+
+};
+
+
+export default styles;
