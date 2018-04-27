ca05d27a6c0ee0624c1dc8d774dd51eb0bd838c1
Liszt Louis <lisztlouis@gmail.com>
2018-04-27 14:15:23 -0400

    Changed the name of the app properly

--- Capture/android/app/src/main/java/com/capture/MainActivity.java
+++ Capture/android/app/src/main/java/com/capture/MainActivity.java
@@ -1,4 +1,4 @@
-package com.capture_2;
+package com.capture;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "capture_2";
+        return "Capture";
     }
 }
