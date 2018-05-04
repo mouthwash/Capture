8e8b43cc9a6325753b43e94b555dc864756e3df3
David <zjdavidwu@gmail.com>
2018-05-04 03:02:30 -0400

    Added NEW DEPENDENCY-> react-native-push-notification, Can notify you of an expired notification but crashes when clicked.

--- Capture/src/components/NewNote.js
+++ Capture/src/components/NewNote.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { TextInput, View } from 'react-native';
 import DateTimePicker from 'react-native-modal-datetime-picker';
 import PopupDialog, { SlideAnimation, DialogTitle } from 'react-native-popup-dialog';
+import PushNotification, { PushNotificationIOS } from 'react-native-push-notification';
 import {
   Container,
   Header,
@@ -28,6 +29,39 @@ export default class NewNote extends Component {
             isDateTimePickerVisible: false,
             paneID: this.props.navigation.state.params.paneID,
         };
+
+        PushNotification.configure({
+
+            // (optional) Called when Token is generated (iOS and Android)
+            onRegister(token) {
+                console.log('TOKEN:', token);
+            },
+
+            // (required) Called when a remote or local notification is opened or received
+            onNotification(notification) {
+                console.log('NOTIFICATION:', notification);
+
+                // process the notification
+
+                // required on iOS only (see fetchCompletionHandler docs: https://facebook.github.io/react-native/docs/pushnotificationios.html)
+                notification.finish(PushNotificationIOS.FetchResult.NoData);
+            },
+
+          // ANDROID ONLY: GCM Sender ID (optional - not required for local notifications,
+          //but is need to receive remote push notifications)
+            senderID: 'YOUR GCM SENDER ID',
+
+            // IOS ONLY (optional): default: all - Permissions to register.
+            permissions: {
+                alert: true,
+                badge: true,
+                sound: true
+            },
+
+            popInitialNotification: true,
+
+            requestPermissions: true,
+        });
     }
 
   showDateTimePicker = () => this.setState({ isDateTimePickerVisible: true });
@@ -111,6 +145,10 @@ export default class NewNote extends Component {
                               };
                               console.log('NEW NOTE =======', newNote);
                               insertNewNote(newNote, this.state.paneID);
+                              PushNotification.localNotificationSchedule({
+                                message: 'Note Expiring', // (required)
+                                date: this.state.date_time
+                              });
                               goBack();
                             }
                           }
